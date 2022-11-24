e88d419139d9ad049becb5b64f94e701c9690bd6
= <=>
2022-11-23 21:49:16 -0600

    Connect to MongoDB and global error handler

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-//Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 //Load env file
 dotenv.config({path: './config/config.env'});
+
+//Connect to DB
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -20,4 +24,15 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, console.log('Server running in: '+process.env.NODE_ENV));
+const server = app.listen(
+    PORT, 
+    console.log('Server running in: '+process.env.NODE_ENV)
+);
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise)=>{
+    console.log('Error: '+err.message);
+
+    //Close server and kill process
+    server.close(() => process.exit(1));
+})
