aa12191afe0fe1fb1f6ecf5d67041b8357b56c8d
= <=>
2022-11-23 20:27:19 -0600

    Create bootcamp router

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 //Load env file
 dotenv.config({path: './config/config.env'});
 
 const app = express();
 
-const PORT = process.env.PORT || 5001;
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
+
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, console.log('Server running in: '+process.env.NODE_ENV));
