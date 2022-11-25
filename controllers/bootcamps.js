d92be7ece5e2ae813de682ed1a91ec03603d1538
= <=>
2022-11-24 20:01:14 -0600

    Custom Error Handler

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp');
 
 //@desc         Get all bootcamps
@@ -13,7 +14,7 @@ exports.getBootcamps = async (req, res, next) => {
             data: bootcamps
         });
     } catch(err){
-        res.status(400).json({ success: false });
+        next(err);
     }
 }
 
@@ -25,7 +26,7 @@ exports.getBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findById(req.params.id);
 
         if(!bootcamp){
-            return res.status(400).json({ success: false });
+            return next(new ErrorResponse("Bootcamp not found with id of "+req.params.id, 404));
         }
 
         res.status(200).json({
@@ -33,7 +34,7 @@ exports.getBootcamp = async (req, res, next) => {
             data: bootcamp
         })
     } catch(err){
-        res.status(400).json({ success: false });
+        next(err);
     }
 }
 
@@ -50,7 +51,7 @@ exports.createBootcamp = async (req, res, next) => {
         });
     }
     catch(err){
-        res.status(400).json({ success: false });
+        next(err);
     }
 }
 
@@ -65,7 +66,7 @@ exports.updateBootcamp = async (req, res, next) => {
         });
     
         if(!bootcamp){
-            return res.status(400).json({ success: false});
+            return next(new ErrorResponse("Bootcamp not found with id of "+req.params.id, 404));
         }
     
         res.status(200).json({ 
@@ -73,7 +74,7 @@ exports.updateBootcamp = async (req, res, next) => {
             data: bootcamp
         });
     } catch (error) {
-        res.status(400).json({ success: false });
+        next(err);
     }
     
 }
@@ -86,7 +87,7 @@ exports.deleteBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
     
         if(!bootcamp){
-            return res.status(400).json({ success: false });
+            return next(new ErrorResponse("Bootcamp not found with id of "+req.params.id, 404));
         }
     
         res.status(200).json({ 
@@ -94,7 +95,7 @@ exports.deleteBootcamp = async (req, res, next) => {
             data: {}
         });
     } catch (error) {
-        res.status(400).json({ success: false });
+        next(err);
     }
 }
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 
 //Load env file
 dotenv.config({path: './config/config.env'});
@@ -25,6 +26,8 @@ if(process.env.NODE_ENV === 'development'){
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
 
 //Serve application
 const PORT = process.env.PORT || 5001;
