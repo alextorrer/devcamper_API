e02380cbcfeb6793f1340a4cb02d94cf4e7b58ac
alextorrer <alextorre0427@gmail.com>
2022-11-29 20:16:29 -0600

    Authorization functionality

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -14,6 +14,8 @@ const courseRouter = require('./courses');
 
 const router = express.Router();
 
+const { protect, authorize } = require('../middleware/auth');
+
 //Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
 
@@ -24,16 +26,16 @@ router
 router
     .route('/')
     .get(getBootcamps)
-    .post(createBootcamp);
+    .post(protect, authorize('publisher', 'admin'), createBootcamp);
 
 router
     .route('/:id')
     .get(getBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp);
+    .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+    .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 router
     .route('/:id/photo')
-    .put(bootcampPhotoUpload);
+    .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 module.exports = router;
